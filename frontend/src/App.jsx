fb733f1a3a532787c7634578717f4cbfb416bf18
thivi400 <e22400@eng.pdn.ac.lk>
2026-02-14 11:45:36 +0530

    Add gitignore and stop tracking frontend node_modules

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home.jsx"
 import About from "./pages/about.jsx"
-function Contact() { return <div className="p-6">Contact</div>; }
+import Contact from "./pages/contact.jsx"
 function Tourism() { return <div className="p-6">Tourism</div>; }
 function Delights() { return <div className="p-6">Delights</div>; }
 function Fancy() { return <div className="p-6">Fancy</div>; }
